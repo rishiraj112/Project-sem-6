83edbaa94f205f4b0fe77d9775f8cd3f7c4e477a
rishiraj112 <rishikumarsingh06185@gmail.com>
2023-07-19 20:00:10 +0530

    112

--- connect.php
+++ connect.php
@@ -1,33 +1,25 @@
 <?php
-$servername="localhost";
-$username="root";
-$password="";
-$database_name="customer";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database_name = "customer";
 
-$conn=mysqli_connect($servername,$username,$password,$database_name);
+$conn = mysqli_connect($servername, $username, $password, $database_name);
 //check the connection
 
-if(!$conn)
-{
+if (!$conn) {
     die("Connection Failed:" . mysqli_connect_error());
 }
-if(isset($_POST['save']))
-{
-    $username = $_POST['$username'];
-    $email = $_POST['$email'];
-    $password = $_POST['$password'];
-    $sql_query = "INSERT INFO entry_details(username,email,password)
-    VALUES ('username','email','password')";
-    if(mysqli_query($conn,$sql_query))
-    {
-        echo "New Details Entry inserted successfully !";
-    }
-    else
-    {
-        echo "Error".$sql . "" .mysqli_error($conn);
-    }
-    mysqli_close($conn);
 
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$sql_query = "INSERT INTO entry_details VALUES ('$username','$email','$password')";
+if (mysqli_query($conn, $sql_query)) {
+    echo "New Details Entry inserted successfully !";
+} else {
+    echo "Error" . $sql . "" . mysqli_error($conn);
 }
+mysqli_close($conn);
 
 ?>
